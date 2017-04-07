c59dcb313bfee89ce06c36614f84e685bb84de66
CONNOR <ckirk96@gmail.com>
2017-04-07 12:53:14 -0400

    added error handling

--- kirk.c
+++ kirk.c
@@ -3,15 +3,25 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <errno.h>
 
 struct termios og_termios;
 
+void die(const char *s){
+	perror(s);
+	exit(EXIT_FAILURE);
+}
+
 void disableRawMode(){
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &og_termios);
+	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &og_termios) == -1){
+		die("tcsetattr");
+	}
 }
 
 void enableRawMode(){
-	tcgetattr(STDIN_FILENO, &og_termios);
+	if(tcgetattr(STDIN_FILENO, &og_termios) == -1){
+		die("tcgetattr");
+	}
 	atexit(disableRawMode);	
 	
 	struct termios raw = og_termios;
@@ -22,7 +32,9 @@ void enableRawMode(){
 	raw.c_cc[VMIN] = 0;
 	raw.c_cc[VTIME] = 1;
 	
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1){
+		die("tcsetattr");
+	}
 }
 
 int main(){
@@ -31,7 +43,10 @@ int main(){
 	char c;
 	while( 1 ){
 		c = '\0';
-		read(STDIN_FILENO, &c, 1);
+		if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN){
+			die("read");
+		}
+	
 		if( iscntrl(c) ){
 			printf("%d\r\n", c);
 		}else{
