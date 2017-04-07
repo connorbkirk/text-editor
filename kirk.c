f6003408280e5bd99be961dfb9c9d71e73eff30a
CONNOR <ckirk96@gmail.com>
2017-04-07 12:08:36 -0400

    enable/disable raw mode

--- kirk.c
+++ kirk.c
@@ -1,6 +1,26 @@
 #include <unistd.h>
+#include <termios.h>
+#include <stdlib.h>
+
+struct termios og_termios;
+
+void disableRawMode(){
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &og_termios);
+}
+
+void enableRawMode(){
+	tcgetattr(STDIN_FILENO, &og_termios);
+	atexit(disableRawMode);	
+	
+	struct termios raw = og_termios;
+	raw.c_lflag &= ~(ECHO);
+	
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main(){
+	enableRawMode();
+
 	char c;
 	while( read(STDIN_FILENO, &c, 1) == 1 );
 	return 0;
