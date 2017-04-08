c745310973739a48a5a7645d83f90b7457623dfa
CONNOR <ckirk96@gmail.com>
2017-04-08 11:38:15 -0400

    added HOME and END keys

--- kirk.c
+++ kirk.c
@@ -19,6 +19,8 @@ enum editorKey{
 	ARROW_RIGHT,
 	ARROW_UP,
 	ARROW_DOWN,
+	HOME_KEY,
+	END_KEY,
 	PAGE_UP,
 	PAGE_DOWN
 };
@@ -92,10 +94,18 @@ int editorReadKey(){
 					return '\x1b';
 				if(seq[2] == '~'){
 					switch(seq[1]){
+						case '1':
+							return HOME_KEY;
+						case '4':
+							return END_KEY;
 						case '5':
 							return PAGE_UP;
 						case '6':
 							return PAGE_DOWN;
+						case '7':
+							return HOME_KEY;
+						case '8':
+							return END_KEY;
 					}
 				}
 			}else{
@@ -108,10 +118,22 @@ int editorReadKey(){
 						return ARROW_RIGHT;
 					case 'D':
 						return ARROW_LEFT;
+					case 'H':
+						return HOME_KEY;
+					case 'F':
+						return END_KEY;
 				}
 			}
 		}
-		
+		else if(seq[0] == '0'){
+			switch(seq[1]){
+				case 'H':
+					return HOME_KEY;
+				case 'F':
+					return END_KEY;
+			}
+		}
+	
 		return '\x1b';
 	}
 
@@ -253,6 +275,14 @@ void editorProcessKeypress(){
 			write(STDOUT_FILENO, "\x1b[2J", 4);
 			write(STDOUT_FILENO, "\x1b[H", 3);
 			exit(EXIT_SUCCESS);
+			break;
+		
+		case HOME_KEY:
+			E.cx = 0;
+			break;
+		
+		case END_KEY:
+			E.cx = E.screencols - 1;
 			break;
 
 		case PAGE_UP:
